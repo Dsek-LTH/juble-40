37b4945d9fd7a82e4aae72b74c1661cc8a8f95b1
André Philipsson <andre@andrephilipsson.com>
2022-06-09 21:44:59 +0200

    add genie

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,28 @@
-import Link from 'next/link'
 import Image from 'next/image'
 import Layout from '../components/Layout'
+import Panter from "../public/assets/images/pink-panther-pointing-up.webp"
 
 import Logo from "../public/assets/images/juble40_color.svg"
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <div className="h-[5000px]">
+    <div className="h-[5000px] pt-8">
       <div className="flex justify-center">
         <div className="flex flex-col">
           <Image src={Logo} height={250} />
 
           <div className="pt-8 space-y-2">
             <h1 className="text-3xl font-semibold text-center">Jubileum blah blah blah</h1>
-            <h3 className="text-lg text-center">1 Juni till 7 Juni</h3>
+            <h3 className="text-lg text-center">11 November &mdash; 19 November</h3>
           </div>
         </div>
       </div>
 
       <button
-        className="block fixed h-16 w-16 right-8 bottom-8 bg-blue-500 text-white rounded-full"
-        onClick={() => window.scrollTo(0, 0)}
+        className="block fixed h-16 w-16 right-4 bottom-4"
+        onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
       >
-        Top
+        <Image src={Panter} />
       </button>
     </div>
   </Layout>
