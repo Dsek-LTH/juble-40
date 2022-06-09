89c8eabaae9bac9f0f7d5839fb6efd9675042917
André Philipsson <andre@andrephilipsson.com>
2022-06-09 21:16:23 +0200

    update layout

---
+++ pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from "next/document"
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body className="min-h-screen bg-neutral-900 text-white scroll-smooth">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
